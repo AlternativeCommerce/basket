93943638e3230376870a9e272a683dc53489c54d
AlternativeCommerce <94062771+AlternativeCommerce@users.noreply.github.com>
2021-11-10 17:43:11 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AlternativeCommerce'
+copyright = '2021, AlternativeCommerce'
+author = 'AlternativeCommerce'
 
 release = '0.1'
 version = '0.1.0'
